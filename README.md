8940837523dd405b8d88e9b78916708e594d365b
danieldsouza <ddsouza@umich.edu>
2020-05-20 22:54:40 -0400

    # Updating the App & Readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# DeepLearning_CookieCutter
-Aimed to be a Cookie Cutter for all Deep Learning Projects
+# Bi-Directional Attention Flow for Machine Comprehension
+This is a :fire: PyTorch :fire: implementation of the seminal BiDAF Paper from Allen AI(***https://arxiv.org/pdf/1611.01603.pdf***) 
+
+## Architecture
+
+![BIDAF Architecture](https://raw.githubusercontent.com/dsouzadaniel/BiDAF/master/bidaf_model_architecture.png)
+
+My modifications include :neckbeard: :rocket: :
+* QA on a token level instead of a character level ( I use Spacy 2.1.9 to create the dataset from SQUAD )
+* Swapping out GloVe for Elmo for improved performance 
+
+
+## Dataset
+I have uploaded a sample dataset from the SQUAD Dataset.
+Download the complete dataset at [SQUAD QA Dataset](https://rajpurkar.github.io/SQuAD-explorer/)
+
+
+## Demo
+:bowtie: I have also implemented a Streamlit App to interact with the model! :bowtie:
+
+![Streamlit App](https://raw.githubusercontent.com/dsouzadaniel/BiDAF/master/bidaf_model_app.png)
+
+
+## Instructions 
+
+#### To Install Requirements
+
+> `pip install -r requirements.txt`
+
+#### To Train a New Model
+
+> ` python train.py`
+
+#### To Run the Demo Streamlit App
+ 
+> ` streamlit run app.py`
+
+## Improvements
+:v: Pull requests are welcome for any improvements/features :v:
+
--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from model import architecture
 from utils import helper
 
 # Default Args
-default_context = "The food was great. The service was quick and the staff was very polite."
-default_query = "how was the service ?"
+default_context = "The Mask of Tutankhamun is a gold mask of the 18th-dynasty ancient Egyptian Pharaoh Tutankhamun (reigned 1332–1323 BC). It was discovered by Howard Carter in 1925 in tomb KV62 in the Valley of the Kings, and is now housed in the Egyptian Museum in Cairo."
+default_query = "When was the Mask of Tutankhamun discovered ?"
 
 
 @st.cache(allow_output_mutation=True)
@@ -23,11 +23,11 @@ def load_model_for_app():
 
 pretrained_BIDAF = load_model_for_app()
 
-st.title('Bi-Directional Attn Flow(BiDAF) Demo')
+st.title('Bidirectional Attention Flow(BiDAF) Demo')
 
 st.header('Input')
-context_text = st.text_input(label='Enter Context Here',
-                             value=default_context)
+context_text = st.text_area(label='Enter Context Here',
+                            value=default_context)
 query_text = st.text_input(label='Enter Query Here',
                            value=default_query)
 
@@ -37,6 +37,6 @@ if len(context_text) == 0 or len(query_text) == 0:
 
 highlighted_context, confidence = helper.predict(context_text, query_text, pretrained_BIDAF)
 
-st.subheader('Predicted Span')
-st.write("Answering with Confidence {0}".format(round(confidence,3)))
+st.header('Output')
+st.subheader('Predicted Span ( Confidence : {0} )'.format(round(confidence, 2)))
 st.markdown(highlighted_context, unsafe_allow_html=True)
